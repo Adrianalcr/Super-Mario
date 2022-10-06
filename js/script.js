d6a27e343b9f2c56f5078eec29e983c34345eed4
Adriana Lima <adrianalima_webdesigner@outlook.com>
2022-10-06 11:57:33 -0300

    Update pulo mobile

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ const pipe = document.querySelector('.pipe');
 //Mario Pula com o click de qualquer tecla.
 const pulo = () =>{  //Dispara a função do pulo
     mario.classList.add('pulo'); // Inicia a animação pulo
-
+    
     setTimeout(() =>{
         mario.classList.remove('pulo'); // Aciona novamente o pulo
     }, 500);
+
 }
 
 
@@ -15,7 +16,7 @@ const pulo = () =>{  //Dispara a função do pulo
 const loop = setInterval(() =>{
     const musica = document.getElementById('musica');
     musica.play();
-
+        
     const pipePosition = pipe.offsetLeft;
     const marioPosition = window.getComputedStyle(mario).bottom.replace('px', ''); 
 
@@ -25,17 +26,26 @@ const loop = setInterval(() =>{
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`; 
+        
 
         mario.src='./img/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
         
-        alert("GAME OVER");
-        musica.src='./sons/mario_death.wav';
+        function stopMusicaStop(){
+            alert("GAME OVER");
+            musica.src='./sons/mario_death.wav';
+        }stopMusicaStop();     
         
         clearInterval(loop);        
     }
 
+    document.addEventListener("touchstart", e =>{
+        mario.classList.add('pulo');
+    } );
+
 },10)
+
+
 
 document.addEventListener('keydown', pulo) 
